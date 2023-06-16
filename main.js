ffb779e87ed3cf2d66974e2d7bdc03b574863f8c
Empereur <bartosikpatrick2@gmail.com>
2023-06-16 13:08:17 +0100

    🎨 style(main.js) : ajouter des fichiers PDF Les fichiers PDF ont été ajoutés pour permettre aux utilisateurs de télécharger les CV des membres de l'équipe.

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import "./pdf/ANAIS.pdf"
+import "./pdf/Antoine.pdf"
+import "./pdf/Arthur.pdf"
+import "./pdf/Corentin.pdf"
+import "./pdf/Edouard.pdf"
+import "./pdf/Quentin.pdf"
+import "./pdf/Samir.pdf"
+import"./pdf/Soren.pdf"
+import "./pdf/Thibault.pdf"
+import "./pdf/Youssef.pdf"
 
 document.querySelector('#app').innerHTML = `
   <div>
