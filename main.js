459e1ff91c648ca71dabca2f034fd28392d16430
Empereur <bartosikpatrick2@gmail.com>
2023-06-16 13:28:05 +0100

    :memo: update

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 import "/ANAIS.pdf"
-import "/pdf/Antoine.pdf"
-import "/pdf/Arthur.pdf"
-import "/pdf/Corentin.pdf"
-import "/pdf/Edouard.pdf"
-import "/pdf/Quentin.pdf"
-import "/pdf/Samir.pdf"
-import "/pdf/Soren.pdf"
-import "/pdf/Thibault.pdf"
-import "/pdf/Youssef.pdf"
+import "/Antoine.pdf"
+import "/Arthur.pdf"
+import "/Corentin.pdf"
+import "/Edouard.pdf"
+import "/Quentin.pdf"
+import "/Samir.pdf"
+import "/Soren.pdf"
+import "/Thibault.pdf"
+import "/Youssef.pdf"
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -22,6 +22,16 @@ document.querySelector('#app').innerHTML = `
 
     <h1>Hello Vite!</h1>
     <a href="./ANAIS.pdf" target="_blank">ANAIS</a>
+    <a href="./Antoine.pdf" target="_blank">Antoine</a>
+    <a href="./Arthur.pdf" target="_blank">Arthur</a>
+    <a href="./Corentin.pdf" target="_blank">Corentin</a>
+    <a href="./Edouard.pdf" target="_blank">Edouard</a>
+    <a href="./Quentin.pdf" target="_blank">Quentin</a>
+    <a href="./Samir.pdf" target="_blank">Samir</a>
+    <a href="./Soren.pdf" target="_blank">Soren</a>
+    <a href="./Thibault.pdf" target="_blank">Thibault</a>
+    <a href="./Youssef.pdf" target="_blank">Youssef</a>
+    
   </div>
 `
 
