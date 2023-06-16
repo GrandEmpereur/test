d5e97bc72d22cf33d7317f05da298dcab71872ae
Empereur <bartosikpatrick2@gmail.com>
2023-06-16 13:13:39 +0100

    :memo: update

--- main.js
+++ main.js
@@ -9,25 +9,13 @@ import "./pdf/Corentin.pdf"
 import "./pdf/Edouard.pdf"
 import "./pdf/Quentin.pdf"
 import "./pdf/Samir.pdf"
-import"./pdf/Soren.pdf"
+import "./pdf/Soren.pdf"
 import "./pdf/Thibault.pdf"
 import "./pdf/Youssef.pdf"
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <a href="./pdf/ANAIS.pdf">a</a>
   </div>
 `
 
